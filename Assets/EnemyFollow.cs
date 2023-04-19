438a7f9e260accb8dc753319189672b90add3da6
ilyamed <0ilya@bk.ru>
2023-04-19 20:07:01 +0300

    add zombie, try add navmesh following

--- Assets/EnemyFollow.cs
+++ Assets/EnemyFollow.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyFollow : MonoBehaviour
 {
-    public int speed;
-    public Transform player;
+    private NavMeshAgent enemy;
+    private Transform player; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("PlayerCapsule").transform;
+        enemy = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // enemy.SetDestination(Player.position);
-        transform.LookAt(player.transform);
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        enemy.SetDestination(player.position);
+        // transform.LookAt(player.transform);
+        // transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
     }
 }
